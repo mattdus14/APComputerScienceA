59acd8c9b60bea6bfeacd5636f053328c7cfc434
mattdus14 <mattdus14@gmail.com>
2018-11-30 06:18:30 -0500

    Add files via upload

---
+++ PrimeFactorization.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactorization {
+	
+	
+	public static void primeFactorization(int n) {
+	    while (n%2 == 0)  { 
+	        System.out.print(2 + " * "); 
+	        n = n/2; 
+	    } 
+	  
+	    for (int i = 3; i <= Math.sqrt(n); i = i+2) { 
+	        while (n%i == 0) { 
+	            System.out.print(i+" * "); 
+	            n = n/i; 
+	        } 
+	    } 
+	  
+	    if (n > 2) 
+	        System.out.print(n); 
+	}
+	
+	public static void main(String[] args) {
+		primeFactorization(48);
+	}
+}
